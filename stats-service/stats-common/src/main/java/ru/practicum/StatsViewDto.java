748c6d8d74edff0d447e1b8aa88b227ca73f752d
kpak292 <konstantinpak89@gmail.com>
2025-02-11 22:25:07 +0500

    1) Added DTOs 2) Added schema.sql with database structure 3) README.md updated DB structure

---
+++ stats-service/stats-common/src/main/java/ru/practicum/StatsViewDto.java
@@ -0,0 +1,17 @@
+package ru.practicum;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatsViewDto {
+    String app;
+
+    String uri;
+
+    int hits;
+}
