492f6f6498ec1bc2e4de8db3fe6cf38420211f9d
Lomonosov-Dmitry <cooler7@icloud.com>
2025-02-14 11:31:55 +0300

    Исправлена ошибка формирования ответа если различается количество uri в запросе и полученных из SQL данных

--- stats-service/stats-server/src/main/java/ru/practicum/StatsController.java
+++ stats-service/stats-server/src/main/java/ru/practicum/StatsController.java
@@ -1,7 +1,6 @@
 package ru.practicum;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +11,6 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class StatsController {
 
-    @Autowired
     private final StatsService statsService;
 
     @PostMapping("/hit")
